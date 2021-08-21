cc69ea0401973e9eaff2c5e5d3db82801a0a052d
Lester Roxas <roxas.lester@auf.edu.ph>
2021-08-21 22:12:22 +0800

    Add project files.

---
+++ pdc6_mod1_activity01/pdc6_mod1_activity01/AppConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pdc6_mod1_activity01
+{
+    class AppConstants
+    {
+        public static double NormalFontSize = 14;
+        public static double LargeFontSize = 30;
+
+    }
+}
---
+++ pdc6_mod1_activity01/pdc6_mod1_activity01/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using System.Windows.Input;
+
+namespace pdc6_mod1_activity01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigateCommand = new Command<Type>(async (Type pageType) =>
+            {
+                Page page = (Page)Activator.CreateInstance(pageType);
+                await Navigation.PushAsync(page);
+            });
+            BindingContext = this;
+        }
+        public ICommand NavigateCommand { private set; get; }
+    }
+}
